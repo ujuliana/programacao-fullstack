037cb639b6a9afcb42dfb5bd7d4d67a97bba754c
Juliana <jalmeiida3004@gmail.com>
2023-06-15 21:07:13 -0300

    primeiro html

---
+++ aula-casa/exercicio-if e else.js
@@ -0,0 +1,17 @@
+let idade = 18;
+
+
+if (idade >= 18) {
+  console.log("Pode votar e dirigir");
+} else if (idade >= 16 && idade < 18) {
+   console.log("Pode votar, mas não pode dirigir");
+} else {
+    console.log("Não pode votar nem dirigir")
+}
+
+
+
+
+
+
+// perguntar se tem como fazer apenas com o else if
